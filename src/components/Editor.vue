c07c282939cec7492d95464a24d3601c6b259112
TokiyaHorikawa <ayikot4423@gmail.com>
2019-09-23 22:10:01 +0900

    feat: マークダウンエディタの実装

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,20 +3,41 @@
     <h1>エディター画面</h1>
     <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import marked from "marked"
+
 export default {
   name: "editor",
   props: ["user"],
   data() {
-    return {}
+    return {
+      markdown: ""
+    }
   },
   methods: {
     logout: function () {
       firebase.auth().signOut()
+    },
+    preview: function () {
+      return marked(this.markdown)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.editorWrapper {
+  display: flex;
+}
+.markdown {
+  width: 50%;
+  text-align: left;
+}
+</style>
