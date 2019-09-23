c2f7da473eb167af5f1928371781a3462d162a60
TokiyaHorikawa <ayikot4423@gmail.com>
2019-09-23 16:25:49 +0900

    feat: welcom Home

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home" class="homeContent">
+    <h1>{{ msg }}</h1>
+    <button>Googleアカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcom to Mymarkdown test"
+    }
+  }
+}
+</script>
+
+<style>
+.homeContent {
+  text-align: center;
+}
+</style>
